6ec30453a6ddc7e2ceca83630afeec93da6dba6f
Henrique Batista <henrique.freitas.batista@sibs.com>
2023-02-24 10:49:22 +0000

    Initial Commit

---
+++ Sources/CalendarControl.xcframework/ios-arm64/CalendarControl.framework/Headers/CalendarControl.h
@@ -0,0 +1,18 @@
+//
+//  CalendarControl.h
+//  CalendarControl
+//
+//  Created by Henrique Freitas Batista on 23/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarControl.
+FOUNDATION_EXPORT double CalendarControlVersionNumber;
+
+//! Project version string for CalendarControl.
+FOUNDATION_EXPORT const unsigned char CalendarControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarControl/PublicHeader.h>
+
+
